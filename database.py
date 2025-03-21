6bd26063f977df05b62cfa07850e1603dfd5039b
jean-baptiste.guerin <jean-baptiste.guerin@jellyfish.com>
2025-03-21 15:59:48 +0100

    feat: intialize db sqllite

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-#from models.sql_models import Base
+from models.sql_models import Base
 import os
 
 load_dotenv()
@@ -8,4 +8,4 @@ database_url = os.getenv("DATABASE_URL")
 
 engine = create_engine(database_url, echo=True)
 
-#Base.metadata.create_all(engine)
+Base.metadata.create_all(engine)
